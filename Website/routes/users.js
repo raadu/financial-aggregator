6889788bec56f96c30e40d3952667ca66db99e33
raadu <raiyad.raad@gmail.com>
2018-12-16 01:13:03 +0600

    logout added

--- Website/routes/users.js
+++ Website/routes/users.js
@@ -95,4 +95,12 @@ router.post('/login',
 		res.redirect('/');
 });
 
+router.get('/logout', function (req, res) {
+	req.logout();
+
+	req.flash('success_msg', 'You are logged out');
+
+	res.redirect('/users/login');
+});
+
 module.exports = router;
