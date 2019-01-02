6b04a3cfb6da50a4b9a2befd44c137acbc3d0654
raadu <raiyad.raad@gmail.com>
2019-01-02 13:35:42 +0600

    added pages for loans,fixed deposits,dfs

--- Website/routes/index.js
+++ Website/routes/index.js
@@ -16,4 +16,20 @@ router.get('/contact', function(req, res){
 	res.render('contact');
 });
 
+//Get Loan List page
+router.get('/loanlist', function(req, res){
+	res.render('loanlist');
+});
+
+//Get Fixed Deposit List page
+router.get('/fixeddepositlist', function(req, res){
+	res.render('fixeddepositlist');
+});
+
+//Get Loan List page
+router.get('/dfslist', function(req, res){
+	res.render('dfslist');
+});
+
+
 module.exports = router;
