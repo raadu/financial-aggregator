eca3c1dea4f8bbbfb5fdd5adef915c35dc5ebb23
raadu <raiyad.raad@gmail.com>
2019-03-11 04:38:14 +0600

    sorting algorithm added

--- Website/routes/creditcards.js
+++ Website/routes/creditcards.js
@@ -27,4 +27,40 @@ router.get('/cardlist', function(req, res, next) {
   });
 });
 
+//sort cards by Interest Rates
+router.get('/sortByInterestRate', function(req, res, next) {
+  var resultArray = [];
+  mongoose.connect(url, { useNewUrlParser: true }, function(err, db) {
+    assert.equal(null, err);
+    var sort = {interestRateMonthly: 1};
+    var cursor = db.collection('creditcards').find().sort(sort); //inside mongoDB, collection name in 'creditcards'
+    cursor.forEach(function(doc, err) {
+      assert.equal(null, err);
+      resultArray.push(doc);
+    }, function() {
+      db.close();
+      res.render('cardlist', {items: resultArray}); //rendered in 'cardlist.handlebars' view
+    });
+  });
+});
+
+//sort cards by Annual Fees
+router.get('/sortByAnnualFee', function(req, res, next) {
+  var resultArray = [];
+  mongoose.connect(url, { useNewUrlParser: true }, function(err, db) {
+    assert.equal(null, err);
+    var sort = {annualFee: 1};
+    var cursor = db.collection('creditcards').find().sort(sort); //inside mongoDB, collection name in 'creditcards'
+    cursor.forEach(function(doc, err) {
+      assert.equal(null, err);
+      resultArray.push(doc);
+    }, function() {
+      db.close();
+      res.render('cardlist', {items: resultArray}); //rendered in 'cardlist.handlebars' view
+    });
+  });
+});
+
+
+
 module.exports = router;
