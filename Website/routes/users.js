731c01f26dc9efb77b3beaca1ffa8e78a9314cd4
raadu <raiyad.raad@gmail.com>
2019-04-11 07:18:10 +0600

    profile page minor edit

--- Website/routes/users.js
+++ Website/routes/users.js
@@ -41,6 +41,9 @@ router.get('/userlist', function(req, res, next) {
   });
 });
 
+//Retrieve logged in user profile info
+
+
 //Show editprofile
 router.get('/editprofile', function(req, res){
 	res.render('editprofile');
