a94ab66dea1a3304ee546c736ffe3c2282560c33
raadu <raiyad.raad@gmail.com>
2020-05-26 22:27:30 +0600

    node modules removed and package version updated

---
+++ app.js
@@ -0,0 +1,99 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/fintech', { useNewUrlParser: true }, (err) => {
+    if (!err) { console.log('MongoDB Connection Succeeded.') }
+    else { console.log('Error in DB connection : ' + err) }
+});
+mongoose.set('useCreateIndex', true);
+var db = mongoose.connection;
+
+// declare route for index.js file in routes folder
+var routes = require('./routes/index');
+// declare routes for other js files in routes folder
+var users = require('./routes/users');
+var creditcards = require('./routes/creditcards');
+var loans = require('./routes/loans');
+var fds = require('./routes/fds');
+
+
+//init app
+var app = express();
+
+//view engine
+app.set('views',path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout: 'layout'}));
+app.set('view engine', 'handlebars');
+
+// BodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//express session
+app.use(session({
+	secret: 'secret',
+	saveUninitialized: true,
+	resave: true
+})); 
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// Connect Flash
+app.use(flash());
+
+// Global Vars
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
+});
+
+// using above declared route variables 
+app.use('/', routes); //for index.js file in routes folder
+app.use('/users', users); //for users.js file
+app.use('/creditcards', creditcards); //for creditCards.js file
+app.use('/loans', loans); //for loans.js file
+app.use('/fds', fds); //for fds.js file
+
+
+// Set Port
+app.set('port', (process.env.PORT || 3000));
+
+app.listen(app.get('port'), function(){
+	console.log('Server started on port '+app.get('port'));
+});
